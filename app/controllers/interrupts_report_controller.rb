c4ead4f1156244101689d676438216dbfe2e7b9d
eugene <bannikov.evgeny@gmail.com>
2013-04-15 21:35:55 +0600

    mend
    Version_0.0.1

--- app/controllers/interrupts_report_controller.rb
+++ app/controllers/interrupts_report_controller.rb
@@ -1,4 +1,8 @@
+#encoding:utf-8
+require 'zip/zip'
 class InterruptsReportController < ApplicationController
+
+  include ReportsFuncs
 
   layout "ir"
 
@@ -8,27 +12,40 @@ class InterruptsReportController < ApplicationController
   #POST /ir/get_result
   def get_result
 
-    tp_report_file = params[:tp_report].tempfile unless params[:tp_report].blank?
-    fp_report_file = params[:fp_report].tempfile unless params[:fp_report].blank?
-    pp_report_file = params[:pp_report].tempfile unless params[:pp_report].blank?
-    gl_report_file = params[:gl_report].tempfile unless params[:gl_report].blank?
-
     data_source = {};
-    data_source[:tp_report] = LmTpReport.new(tp_report_file.read) unless tp_report_file.nil?
-    data_source[:fp_report] = LmFpReport.new(fp_report_file.read) unless fp_report_file.nil?
-    data_source[:pp_report] = LmPpReport.new(pp_report_file.read) unless pp_report_file.nil?
-    data_source[:gl_report] = LmGlReport.new(gl_report_file.read) unless gl_report_file.nil?
+
+    case params[:data_type]
+    when "zip"
+
+      if params[:zip_file]
+        zip_file = params[:zip_file].tempfile
+
+        tz = Zip::ZipFile.open(zip_file)
+
+        data_source = load_reports_from_zip tz, LmInterruptsReport.new.data_source_shema
+      end
+
+    else
+      tp_report_file = params[:tp_report].tempfile unless params[:tp_report].blank?
+      fp_report_file = params[:fp_report].tempfile unless params[:fp_report].blank?
+      pp_report_file = params[:pp_report].tempfile unless params[:pp_report].blank?
+      gl_report_file = params[:gl_report].tempfile unless params[:gl_report].blank?
+
+      data_source[:tp_report] = LmTpReport.new(tp_report_file.read) unless tp_report_file.nil?
+      data_source[:fp_report] = LmFpReport.new(fp_report_file.read) unless fp_report_file.nil?
+      data_source[:pp_report] = LmPpReport.new(pp_report_file.read) unless pp_report_file.nil?
+      data_source[:gl_report] = LmGlReport.new(gl_report_file.read) unless gl_report_file.nil?
+    end
 
     ir = LmInterruptsReport.new source_reports: data_source,
                                       options: {
-                                        #rayon: @report_rayon,
-                                        report_date: Date.strptime( '12.04.2013', '%d.%m.%Y')
+                                        rayon: params[:rayon],
+                                        report_date: Date.today
                                       }
 
     flash[:interrupts_report] = ir
 
     #flash[:debug] = data_source[:pp_report]
-
     redirect_to action: "result"
   end
 
